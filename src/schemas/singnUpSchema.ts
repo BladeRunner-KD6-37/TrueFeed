e49f684f48e6352efd424456022a58a7f9037207
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-06-23 16:54:32 +0530

    schemas created for validation:

--- src/schemas/singnUpSchema.ts
+++ src/schemas/singnUpSchema.ts
@@ -7,5 +7,7 @@ export const usernameValidation = z
     .regex(/[^a-zA-Z0-9]/, "username must not contain special character")
 
 export const signUpSchema = z.object({
-    
+    username : usernameValidation,
+    email : z.string().email({message: 'Invalid email address'}),
+    password  : z.string().min(6, {message : "password must be atleast 6 characters"})
 })
