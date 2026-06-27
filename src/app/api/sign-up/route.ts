1548e78b539ecb394074c37cb6573d0478693915
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-06-27 23:08:05 +0530

    user filter added

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,7 +8,34 @@ export async  function POST(request:Request){
     await dbConnect()
     try {
         const {username, email, password} = await request.json()
+        const existingUserVerifiedByUsername =  await UserModel.findOne({
+            username,
+            isVerified : true
+        })
+
+        if (existingUserVerifiedByUsername){
+            return Response.json({
+                success: false,
+                message : "Username is already taken"
+            }, {status : 400})
+        }
+        const existingUserByEmail = await UserModel.findOne({
+            email,
+            isVerified : true
+        })
         
+        if(existingUserByEmail){
+            // return Response.json({
+            //     success : false,
+            //     message : "Email already registered"
+            // }, {status : 400})
+            true //TODO: here later
+        }
+        else{
+           const hashed = await bcrypt.hash(password,10)
+           const expiryDate = new Date()
+           expiryDate.setHours(expiryDate.getHours()+1)
+        }
     } catch (error) {
         console.error('Error registering user', error)
         return Response.json({ //send to frontend
